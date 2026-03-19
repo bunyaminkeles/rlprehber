bbaa8844272b888d78fa2ac9c7881ca312cfd61b
Bünyamin Keleş <57811279+bunyaminkeles@users.noreply.github.com>
2026-03-19 23:36:56 +0100

    fix: duyurular listesi en fazla 10 duyuru gösteriyor (en yeni önce)

--- duyurular/views.py
+++ duyurular/views.py
@@ -3,10 +3,10 @@ from .models import Duyuru
 
 def liste(request):
     kategori = request.GET.get('kategori', '')
-    qs = Duyuru.objects.filter(yayinda=True)
+    qs = Duyuru.objects.filter(yayinda=True).order_by('-id')
     if kategori:
         qs = qs.filter(kategori=kategori)
-    return render(request, 'duyurular/liste.html', {'duyurular': qs, 'secili': kategori})
+    return render(request, 'duyurular/liste.html', {'duyurular': qs[:10], 'secili': kategori})
 
 def detay(request, pk):
     duyuru = get_object_or_404(Duyuru, pk=pk, yayinda=True)
